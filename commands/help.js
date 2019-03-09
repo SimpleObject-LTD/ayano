21c85e1fc7362ffeab3205cebbdce2edf931ea87
nysean0220 <36288159+nysean0220@users.noreply.github.com>
2019-03-08 19:48:19 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,18 @@
-module.exports = {
- name: 'help',
- description: 'Show Commands',
- async run(client, message, args
-  
-}
+var CMD = require('../Structures/Command');
+
+class Help extends CMD {
+ constructor(client) {
+  super(client, {
+   name: "Help",
+   description: "Show Commands"
+  })
+ }
+ 
+ async run(message, args) {
+  var client = this.client
+ }
+ 
+};
+
+
+module.exports = Help;
